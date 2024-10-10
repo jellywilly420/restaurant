b5f86b93615a982e228e4f51b6e072d435eedaf1
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2024-10-10 22:57:43 +0300

    Add page swapping functionality

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,23 @@
 const content = document.body.querySelector(".content");
+const homeButton = document.body.querySelector("#homePage");
+const menuButton = document.body.querySelector("#menuPage");
+const contactButton = document.body.querySelector("#contactPage");
 
+
+
+function clearContent() {
+    content.innerHTML = "";
+}
 function createElement(elem) {
     return document.createElement(elem);
-};
+}
+function disableButton(button) {
+    button.classList.add("disabled")
+}
+function enableButton(button) {
+    if ([...button.classList].includes("disabled")) {
+        button.classList.remove("disabled")
+    }
+}
 
-export {createElement, content};
+export {createElement, content, clearContent, homeButton, menuButton, contactButton, disableButton, enableButton};
--- src/index.js
+++ src/index.js
@@ -2,12 +2,81 @@ import "./styles.css";
 import {renderHome} from "./home.js";
 import { renderMenu } from "./menu.js";
 import { renderContact } from "./contact.js";
- {renderMenu};
-let tabs = {
-    0: "home",
-    1: "menu", 
-    2: "contact",
-};
-let currentTab = tabs[0];
-//--------------------
-renderContact();
+import { clearContent, homeButton, menuButton, contactButton, disableButton, enableButton} from "./DOM.js";
+
+// -----------------
+
+class Page {
+    constructor(name) {
+        this.name = name;
+    }
+    isLoaded = false;
+}
+
+// ------------------
+
+const homePage = new Page("homePage");
+const menuPage = new Page("menuPage");
+const contactPage = new Page("contactPage");
+
+const pages = [homePage, menuPage, contactPage];
+const buttons = [homeButton, menuButton, contactButton];
+
+// ------------------
+
+function getLoadedPage(pages) {
+    for (const i in pages) {
+        if (pages[i].isLoaded) {
+            return pages[i];
+        }
+    }
+}
+
+function getPageByName(pages, name) {
+    return pages.find(page => page.name === name)
+}
+
+function loadPage(page) {
+    page.isLoaded = true;
+}
+
+function unloadPage(page) {
+    page.isLoaded = false;
+}
+
+function renderPage(page) {
+    if (page === homePage) {
+        renderHome();
+    }
+    if (page === menuPage) {
+        renderMenu();
+    }
+    if (page === contactPage) {
+        renderContact();
+    }
+}
+
+// -------------------
+// console.log(buttons);
+for (const i in buttons) {
+    buttons[i].addEventListener('click', () => {
+        if (getLoadedPage(pages).name != buttons[i].id) {
+            
+            unloadPage(getLoadedPage(pages))
+            loadPage(getPageByName(pages, buttons[i].id));
+            clearContent();
+            renderPage(getLoadedPage(pages));
+
+            for (const j in buttons) {
+                enableButton(buttons[j]);
+            }
+            disableButton(buttons[i]);
+        }
+    })
+}
+
+// -------------------
+clearContent();
+loadPage(homePage);
+renderPage(homePage);
+disableButton(homeButton);
