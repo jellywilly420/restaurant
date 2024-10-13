066e86d6b1bb0bcf2a6c8fe92eb3de888f2d62b8
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2024-10-13 03:24:33 +0300

    Add assets Style nav bar Style hero section and special section (mobile only)

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import {createElement, content} from "./DOM.js";
-import tempImg from "./temp.jpg"
+import heroImg from "./assets/pancakes-with-blueberries.jpg"
+import specialImg from "./assets/thickcakes.jpg"
 export {renderHome};
 
 function renderHome() {
@@ -9,11 +10,14 @@ function renderHome() {
     
     const heroImage = createElement("img");
     heroImage.classList.add("hero-image");
-    heroImage.src = tempImg;
+    heroImage.src = heroImg;
 
     const heroText = createElement("h2");
     heroText.classList.add("hero-text");
-    heroText.innerText = "Hello! this is some temporary text. love you :3";
+    heroText.innerHTML = `Welcome to <span class="title">STACKED</span>!<br>Pancakes That Stack Up to Your Cravings! <q>Serving breakfast all day, every day.</q>`;
+
+    const heroFooter = createElement("svg");
+    heroFooter.classList.add("hero-footer");
 
     heroDiv.append(heroImage, heroText);
 
@@ -24,15 +28,15 @@ function renderHome() {
 
     const specialHeader = createElement("h2");
     specialHeader.classList.add("special-header");
-    specialHeader.innerText = "Try our very cool monthly special, the spicy wurst!";
+    specialHeader.innerText = "Try out our October Special: The Thick Stack!";
 
     const specialImage = createElement("img");
     specialImage.classList.add("special-image");
-    specialImage.src = tempImg;
+    specialImage.src = specialImg;
 
     const specialPara = createElement("p");
     specialPara.classList.add("special-para");
-    specialPara.innerText = "trust me bro, it's really freaking cool and tastes good";
+    specialPara.innerText = "Indulge in our ultra-thick, pastry-like pancakes, a decadent twist on the classic stack that’s crispy on the outside, fluffy on the inside, and perfect for fall!";
 
     specialDiv.append(specialHeader, specialImage, specialPara);
 
@@ -49,7 +53,7 @@ function renderHome() {
     // placeholder
     const carouselImage = createElement("img");
     carouselImage.classList.add("carousel");
-    carouselImage.src = tempImg;
+    carouselImage.src = heroImg;
 
     const reviewButton = createElement("button");
     reviewButton.classList.add("review-button");
