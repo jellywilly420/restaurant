61bc03089ca491aca80be9bc3a6e5eabb8748ca3
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2024-10-22 23:20:26 +0300

    Finish menu page - mobile layout

--- src/menu.js
+++ src/menu.js
@@ -56,7 +56,7 @@ function renderMenu() {
         
         const productButton = createElement("button");
         productButton.classList.add("product-button");
-        productButton.innerText = `buy now \n ${productList[i].price}`;
+        productButton.innerHTML = `<span class="buy-tex">Buy now</span><span class="price">${productList[i].price}</span>`;
 
         productDiv.append(imageContainer, productTitle, productAbout, productButton);
 
